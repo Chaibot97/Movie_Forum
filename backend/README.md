556e1f8f7ab0325de55a8bfad3bac28d517784fc
Lizhou Cai <lizhou.cai@yale.edu>
2020-11-25 00:48:53 +0800

    more apis

--- backend/README.md
+++ backend/README.md
@@ -13,9 +13,14 @@ GET\
 - `/movies/LIMIT`
 - `/movies/rating/LIMIT`    sorted by rating
 - `/movies/NAME/LIMIT`      Search by (partial) movie `NAME`
+- `/movies/actor/NAME/LIMIT`      Search by (partial) actor `NAME`
   
 Details for movie with `ID`
 - `/actors/ID`
 - `/keywords/ID`
 - `/genre/ID`
 - `/collection/ID`
+- `/review/ID`
+
+User functions
+- `/review/add/ID/REVIEW/USER`
--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,40 @@ const send_query = (query, res) => {
     .catch(e => console.error(e.stack))
 }
 
+const send_movie_query = (query, res) => {
+  const crewQuery = {
+    text: 'SELECT name, job, profile_url FROM moviedb.movie_crew t, moviedb.crew WHERE movie_id = $1 and crew_id = id',
+  };
+  const castQuery = {
+    text: 'SELECT name, character, profile_url FROM moviedb.movie_cast t, moviedb.actor WHERE movie_id = $1 and actor_id = id ORDER BY t.order',
+  };
+  db.query(query)
+    .then((data) => {
+      return Promise.all(data.rows.map((r) => {
+        const new_r = r;
+        castQuery.values = [r.id];
+        return db.query(castQuery)
+          .then((castData) => {
+            new_r.actors = castData.rows;
+            return new_r;
+          });
+      }));
+    })
+    .then((data) => {
+      return Promise.all(data.map((r) => {
+        const new_r = r;
+        crewQuery.values = [r.id];
+        return db.query(crewQuery)
+          .then((crewData) => {
+            new_r.crews = crewData.rows;
+            return new_r;
+          });
+      }));
+    })
+    .then((data)=>res.send(data))
+    .catch(e => console.error(e.stack))
+}
+
 app.get('/movies/:limit', (req, res) => {  
   console.log('GET', req.originalUrl);
   const { limit } = req.params;
@@ -39,7 +73,8 @@ app.get('/movies/:limit', (req, res) => {
     text: 'SELECT id,vote_avg, vote_count, poster_url, title, language, overview FROM moviedb.movie ORDER BY id DESC LIMIT $1',
     values: [limit],
   };
-  send_query(query, res);
+
+  send_movie_query(query, res);
 })
 
 app.get('/movies/rating/:limit', (req, res) => {  
@@ -49,7 +84,7 @@ app.get('/movies/rating/:limit', (req, res) => {
     text: 'SELECT id,vote_avg,vote_count,  poster_url, title, language, overview FROM moviedb.movie WHERE vote_count>100 ORDER BY vote_avg DESC LIMIT $1',
     values: [limit],
   };
-  send_query(query, res);
+  send_movie_query(query, res);
 })
 
 app.get('/movies/:name/:limit', (req, res) => {  
@@ -61,7 +96,22 @@ app.get('/movies/:name/:limit', (req, res) => {
     ORDER BY id DESC LIMIT $1`,
     values: [limit],
   };
-  send_query(query, res);
+  send_movie_query(query, res);
+})
+
+app.get('/movies/actor/:name/:limit', (req, res) => {  
+  console.log('GET', req.originalUrl);
+  const { name, limit } = req.params;
+  const query = {
+    text: `SELECT m.id,vote_avg,vote_count,  poster_url, title, language, overview 
+    FROM moviedb.movie m 
+    JOIN moviedb.movie_cast c ON m.id = c.movie_id 
+    JOIN moviedb.actor a ON c.actor_id = a.id
+    WHERE LOWER(a.name) like \'%${name.toLowerCase()}%\' 
+    ORDER BY id DESC LIMIT $1`,
+    values: [limit],
+  };
+  send_movie_query(query, res);
 })
 
 app.get('/actors/:id', (req, res) => {  
@@ -101,6 +151,28 @@ app.get('/collection/:id', (req, res) => {
     text: 'SELECT name, poster_url, backdrop_url FROM moviedb.movie_collection, moviedb.collection WHERE movie_id = $1 and collection_id = id',
     values: [id],
   };
+  send_query(query, res);
+})
+
+app.get('/review/:id', (req, res) => {  
+  console.log('GET', req.originalUrl);
+  const { id } = req.params;
+  const query = {
+    text: 'SELECT review, user_name FROM moviedb.movie_review, moviedb.review WHERE movie_id = $1 and review_id = id',
+    values: [id],
+  };
+  send_query(query, res);
+})
+
+app.get('/review/add/:id/:review/:user', (req, res) => {  
+  console.log('GET', req.originalUrl);
+  const { id,review,user } = req.params;
+  const query = {
+    text: `WITH isrt AS (INSERT INTO moviedb.review (review, user_name) VALUES ('${review}','${user}') RETURNING *)
+    INSERT INTO moviedb.movie_review (movie_id, review_id )
+        VALUES (${id}, (SELECT id from isrt)) RETURNING *`,
+  };
+
   send_query(query, res);
 })
 
